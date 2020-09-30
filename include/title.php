10d7a05a25278df1395351e6c04d3c00adad089c
Konstantin Klimenko <klimenko@gk-sneg.co>
2020-09-30 19:04:49 +0500

    2

---
+++ include/title.php
@@ -0,0 +1,1 @@
+<h1> <?$APPLICATION->ShowTitle()?> </h1>
---
+++ site_go/.left.menu.php
@@ -0,0 +1,18 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Home", 
+		"/site_go/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Test", 
+		"/site_go/test.php", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>
